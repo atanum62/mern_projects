51c81fb2142dfe856b8a91d8b34457137f434f67
atanum62 <atanumaity62899@gmial.com>
2025-03-28 17:26:35 +0530

    2 login page done

--- Task_management/frontend/src/App.jsx
+++ Task_management/frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import Home from "./pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/signup" exact element={<SignUp />} />
+    </Routes>
+  </Router>
+);
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <>{routes}</>;
 }
 
 export default App;
--- Task_management/frontend/src/pages/Home/Home.jsx
+++ Task_management/frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      Home
       <h1 class="text-3xl font-bold underline ">Hello world!</h1>
     </div>
   );
