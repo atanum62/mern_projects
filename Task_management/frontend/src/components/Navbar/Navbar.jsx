2bfd57e9325aa416bc45cec66b4fdd6101169d33
atanum62 <atanumaity62899@gmial.com>
2025-03-29 12:51:17 +0530

    3.1 casual commit

--- Task_management/frontend/src/components/Navbar/Navbar.jsx
+++ Task_management/frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
+  const Navigate = useNavigate();
+  const onLogout = () => {
+    Navigate("/login");
+  };
   return (
     <>
       <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+        <SearchBar />
+        <ProfileInfo onLogout={onLogout} />
       </div>
     </>
   );
